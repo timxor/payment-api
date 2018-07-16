7f8102528b6e8b4365a1cc71d04d8b431750bd04
Tim Siwula <tcsiwula@gmail.com>
2018-07-16 00:53:55 -0400

    add bin directory

---
+++ payment_api/src/bin/create_transfer.rs
@@ -0,0 +1,47 @@
+extern crate diesel;
+extern crate payment_api;
+
+use payment_api::*;
+use std::io::{stdin};
+
+fn main() {
+    let connection = establish_connection();
+
+    let mut amount = String::new();
+    let mut currency = String::new();
+    let mut name = String::new();
+    let mut number = String::new();
+    let mut email = String::new();
+    let complete = &false;
+
+
+    println!("How much do you want to transfer?");
+    stdin().read_line(&mut amount).unwrap();
+    let amount = amount.trim_right(); // Remove the trailing newline
+
+
+    println!("What token do you want to use?");
+    stdin().read_line(&mut currency).unwrap();
+    let currency = currency.trim_right(); // Remove the trailing newline
+    
+    println!("what is their name?");
+    stdin().read_line(&mut name).unwrap();
+    let name = name.trim_right(); // Remove the trailing newline
+
+
+    println!("what is their number?");
+    stdin().read_line(&mut number).unwrap();
+    let number = number.trim_right(); // Remove the trailing newline
+
+
+    println!("what is their email?");
+    stdin().read_line(&mut email).unwrap();
+    let email = email.trim_right(); // Remove the trailing newline
+
+    
+    let transfer = create_transfer(&connection, amount, &currency, name, number, email, complete);
+    println!("\n~~~~ Sent transfer ~~~~~");
+    println!("Amount: {} {}", amount, currency);
+    println!("recipient: {}, {}, {}", name, number, email);
+    println!("transaction id: {}\n", transfer.id);
+}
---
+++ payment_api/src/bin/show_transfers.rs
@@ -0,0 +1,33 @@
+extern crate diesel;
+extern crate payment_api;
+
+use payment_api::*;
+use self::models::*;
+use diesel::prelude::*;
+
+fn main() {
+    use self::schema::transfers::dsl::*;
+
+    let connection = establish_connection();
+    let results = transfers
+        .filter(complete.eq(false))
+        .limit(5)
+        .load::<Transfer>(&connection)
+        .expect("Error loading transfers");
+
+    println!("Displaying {} transfers", results.len());
+    let mut index = 1;
+    for transfer in results {
+        
+        println!("\n++++++ transfer id: {} +++++ transfer complete: {} +++++", index, transfer
+            .complete);
+        index = index + 1;
+
+        println!("---------------------------------");
+        println!("amount: {} {}", transfer.amount, transfer.currency);
+        println!("---------------------------------");
+        
+        println!("recipient: {}, {}, {},", transfer.to_name, transfer.to_number, transfer.to_email);
+        println!("---------------------------------");
+    }
+}
